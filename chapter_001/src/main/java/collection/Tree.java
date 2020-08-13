b8b6ec962b5fcbfb6548b94574d05a0a46ed18f9
illia <235illino@gmail.com>
2020-08-13 13:17:41 +0300

    2. Добавить метод boolean isBinary() [#332068]

--- chapter_001/src/main/java/collection/Tree.java
+++ chapter_001/src/main/java/collection/Tree.java
@@ -23,6 +23,21 @@ class Tree<E> implements SimpleTree<E> {
         return rsl;
     }
 
+
+    public boolean isBinary() {
+        boolean isB = true;
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.children.size() > 2) {
+                isB = false;
+                break;
+            }
+        }
+        return isB;
+    }
+
     @Override
     public Optional<Node<E>> findBy(E value) {
         Optional<Node<E>> rsl = Optional.empty();
