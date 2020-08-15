a0d5cb248e0c43b3cab8938ed16fe8a0119bd857
illia <235illino@gmail.com>
2020-08-15 21:23:31 +0300

    4.0. File [#332101]

---
+++ chapter_002/src/main/java/io/Dir.java
@@ -0,0 +1,24 @@
+package io;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("/home/ilya/IdeaProjects/job4j_design");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        for (File f : file.listFiles())
+        {
+           if (f.isFile()) {
+               System.out.println(f.getName() + " " + f.getTotalSpace());
+           }
+        }
+    }
+}
