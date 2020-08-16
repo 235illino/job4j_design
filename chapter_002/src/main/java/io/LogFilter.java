a3e3b8b90963b63a953866030270952068d3a4bc
illia <235illino@gmail.com>
2020-08-16 14:07:42 +0300

    0.3. BufferedReader. [#332099]

---
+++ chapter_002/src/main/java/io/LogFilter.java
@@ -0,0 +1,55 @@
+package io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilter {
+    public static List<String> filter(String file) throws Exception {
+        List<String> lines = new ArrayList<>();
+        List<String> rsl = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            while (in.ready()) {
+                String s = in.readLine();
+                if (is404(s, 2)) {
+                    try {
+                        if (Integer.parseInt(s.split(" ")[getSize(s) - 1]) != 404) {
+                            rsl.add(s);
+                        }
+
+                    } catch (NumberFormatException e) {
+                    }
+
+                }
+            }
+        }
+
+        return rsl;
+    }
+
+    private static boolean is404(String s, int i) throws IOException {
+        return s.split(" ")[getSize(s) - i].contains("404");
+    }
+
+    private static int getSize(String s) throws IOException {
+        return s.split(" ").length;
+
+    }
+
+
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            log.forEach(s -> out.write(s + "\n"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<String> log = filter("log.txt");
+        save(log, "404.txt");
+    }
+}
