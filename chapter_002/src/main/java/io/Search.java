5722d27048e796ad9dfdd2f1964e754ebc23b13b
illia <235illino@gmail.com>
2020-08-16 11:55:52 +0300

    4.1. Сканирование файловой системы. [#332095]

--- chapter_002/src/main/java/io/Search.java
+++ chapter_002/src/main/java/io/Search.java
@@ -4,17 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
 import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get("/home/ilya/IdeaProjects/job4j_design");
+        Path start = Paths.get("./");
         search(start, "txt").forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
-        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().contains(ext));
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
