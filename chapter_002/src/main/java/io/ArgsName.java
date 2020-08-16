5950aaef0d4e38d9cf61a80d40ea76d2fc19e744
illia <235illino@gmail.com>
2020-08-16 21:56:09 +0300

    5.1. Именованные аргументы [#332102]

---
+++ chapter_002/src/main/java/io/ArgsName.java
@@ -0,0 +1,38 @@
+package io;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Please check arg");
+        }
+        for (String s : args) {
+            String[] tmp = s.split("=");
+            values.put(tmp[0].substring(1), tmp[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(jvm.get("out"));
+    }
+}
