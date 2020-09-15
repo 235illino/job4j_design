d73f1f56047a24b62129c609af152fe7c7bb1d12
illia <235illino@gmail.com>
2020-09-15 19:10:48 +0300

    3. Slf4j - вывод переменных. [#332092]

--- chapter_002/src/main/java/io/UsageLog4j.java
+++ chapter_002/src/main/java/io/UsageLog4j.java
@@ -1,16 +1,22 @@
 package io;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class UsageLog4j {
 
-//    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         String name = "Petr Arsentev";
-        int age = 3666;
-        System.out.println(age);
-//        LOG.debug("User info name : {}, age : {}", name, age);
+        int age = 33;
+        byte dateBirth = 5;
+        short monthBirth = 7;
+        long yearBirth = 1990;
+        boolean man = true;
+        float height = 150.0f;
+        double weight = 75.6;
+        LOG.debug("User info name : {}, age : {}, dateBirth : {}, monthBirth : {}, yearBirth : {}, man : {}, " +
+                "height : {}, weight : {}", name, age, dateBirth, monthBirth, yearBirth, man, height, weight);
     }
 }
