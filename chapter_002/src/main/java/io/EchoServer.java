c649b1fe7d653f3da6a7848f7158ed9589049777
illia <235illino@gmail.com>
2020-09-15 18:47:58 +0300

    2.0

--- chapter_002/src/main/java/io/EchoServer.java
+++ chapter_002/src/main/java/io/EchoServer.java
@@ -1,39 +1,142 @@
 package io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class EchoServer {
+//public class EchoServer {
+//
+//    private final static String EXIT = "msg=Exit";
+//    private final static String HELLO = "msg=Hello";
+//
+//    public static void main(String[] args) throws IOException {
+//        try (ServerSocket server = new ServerSocket(9000)) {
+//            boolean isTurnOn = true;
+//            while (isTurnOn) {
+//                Socket socket = server.accept();
+//                try (DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+//                     BufferedReader in = new BufferedReader(
+//                             new InputStreamReader(socket.getInputStream()))) {
+//                    String str;
+//                    while (!(str = in.readLine()).isEmpty()) {
+//                        if (str.contains(HELLO)) {
+//                            out.writeUTF("Hello, dear friend!");
+//                            out.flush();
+//                        } else if (str.contains(EXIT)) {
+//                            isTurnOn = false;
+//                            break;
+//                        } else {
+////                            out.write(str.split("=")[1].replaceFirst(" HTTP/1.1", "").getBytes());
+//                        }
+//                    }
+//
+//                }
+//            }
+//        }
+//    }
+//}
 
-    private final static String EXIT = "msg=Exit";
-    private final static String HELLO = "msg=Hello";
 
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
-            boolean isTurnOn = true;
-            while (isTurnOn) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    String str;
-                    while (!(str = in.readLine()).isEmpty()) {
-                        if (str.contains(HELLO)) {
-                            out.write("Hello, dear friend!".getBytes());
-                        } else if (str.contains(EXIT)) {
-                            isTurnOn = false;
-                            break;
-                        } else {
-                            out.write(str.split("=")[1].replaceFirst(" HTTP/1.1", "").getBytes());
-                        }
-                    }
+public class EchoServer extends Thread
+{
+    // открываемый порт сервера
+    private static final int port   = 6666;
+    private String TEMPL_MSG =
+            "The client '%d' sent me message : \n\t";
+    private String TEMPL_CONN =
+            "The client '%d' closed the connection";
 
+    private  Socket socket;
+    private  int    num;
+
+    public EchoServer() {}
+    public void setSocket(int num, Socket socket)
+    {
+        // Определение значений
+        this.num    = num;
+        this.socket = socket;
+
+        // Установка daemon-потока
+        setDaemon(true);
+        /*
+         * Определение стандартного приоритета главного потока
+         * int java.lang.Thread.NORM_PRIORITY = 5-the default
+         *               priority that is assigned to a thread.
+         */
+        setPriority(NORM_PRIORITY);
+        // Старт потока
+        start();
+    }
+    public void run()
+    {
+        try {
+            // Определяем входной и выходной потоки сокета
+            // для обмена данными с клиентом
+            InputStream  sin  = socket.getInputStream();
+            OutputStream sout = socket.getOutputStream();
+
+            DataInputStream  dis = new DataInputStream (sin );
+            DataOutputStream dos = new DataOutputStream(sout);
+
+            String line = null;
+            while(true) {
+                // Ожидание сообщения от клиента
+                line = dis.readUTF();
+                System.out.println(
+                        String.format(TEMPL_MSG, num) + line);
+                System.out.println("I'm sending it back...");
+                // Отсылаем клиенту обратно эту самую
+                // строку текста
+                dos.writeUTF("Server receive text : " + line);
+                // Завершаем передачу данных
+                dos.flush();
+                System.out.println();
+                if (line.equalsIgnoreCase("quit")) {
+                    // завершаем соединение
+                    socket.close();
+                    System.out.println(
+                            String.format(TEMPL_CONN, num));
+                    break;
                 }
             }
+        } catch(Exception e) {
+            System.out.println("Exception : " + e);
         }
+    }
+    //---------------------------------------------------------
+    public static void main(String[] ar)
+    {
+        ServerSocket srvSocket = null;
+        try {
+            try {
+                int i = 0; // Счётчик подключений
+                // Подключение сокета к localhost
+                InetAddress ia;
+                ia = InetAddress.getByName("localhost");
+                srvSocket = new ServerSocket(port, 0, ia);
+
+                System.out.println("Server started\n\n");
+
+                while(true) {
+                    // ожидание подключения
+                    Socket socket = srvSocket.accept();
+                    System.err.println("Client accepted");
+                    // Стартуем обработку клиента
+                    // в отдельном потоке
+                    new EchoServer().setSocket(i++, socket);
+                }
+            } catch(Exception e) {
+                System.out.println("Exception : " + e);
+            }
+        } finally {
+            try {
+                if (srvSocket != null)
+                    srvSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.exit(0);
     }
 }
--- chapter_002/src/main/java/io/UsageLog4j.java
+++ chapter_002/src/main/java/io/UsageLog4j.java
@@ -5,13 +5,12 @@ import org.slf4j.LoggerFactory;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+//    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 3666;
+        System.out.println(age);
+//        LOG.debug("User info name : {}, age : {}", name, age);
     }
 }
