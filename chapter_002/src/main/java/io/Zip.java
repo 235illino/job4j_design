056c04c31ff01e7b1fda73be3f399d91d1554a53
illia <235illino@gmail.com>
2020-09-22 21:59:36 +0300

    5.2. Архивировать проект [#332106]

--- chapter_002/src/main/java/io/Zip.java
+++ chapter_002/src/main/java/io/Zip.java
@@ -1,12 +1,33 @@
 package io;
 import java.io.*;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    public void packFiles(List<File> sources, File target) {
-
+    public void packFiles(List<Path> sources, File target) {
+        try (FileOutputStream fos = new FileOutputStream(target)) {
+            byte[] buffer = new byte[1024];
+            try (ZipOutputStream zos = new ZipOutputStream(fos)) {
+                for (Path source : sources) {
+                    FileInputStream fis = new FileInputStream(source.toFile());
+                    String path;
+                    if (source.toFile().getAbsolutePath().split(":\\\\").length > 0) {
+                        path = source.toFile().getAbsolutePath().split(":\\\\")[1];
+                    } else {
+                        path = source.toFile().getAbsolutePath();
+                    }
+                    zos.putNextEntry(new ZipEntry(path));
+                    int length;
+                    while ((length = fis.read(buffer)) > 0) {
+                        zos.write(buffer, 0, length);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void packSingleFile(File source, File target) {
