e05ac79edc2c187efa353c9c9bb5b190b433db45
235illino <235illino@gmail.com>
2021-04-15 00:48:34 +0300

    zip_april

--- chapter_002/src/main/java/io/Zip.java
+++ chapter_002/src/main/java/io/Zip.java
@@ -7,22 +7,15 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
     public void packFiles(List<Path> sources, File target) {
-        try (FileOutputStream fos = new FileOutputStream(target)) {
-            byte[] buffer = new byte[1024];
-            try (ZipOutputStream zos = new ZipOutputStream(fos)) {
-                for (Path source : sources) {
-                    FileInputStream fis = new FileInputStream(source.toFile());
-                    String path;
-                    if (source.toFile().getAbsolutePath().split(":\\\\").length > 0) {
-                        path = source.toFile().getAbsolutePath().split(":\\\\")[1];
-                    } else {
-                        path = source.toFile().getAbsolutePath();
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toString())))) {
+            for (Path p : sources) {
+                try {
+                    zip.putNextEntry(new ZipEntry(p.toString()));
+                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(p.toString()))) {
+                        zip.write(out.readAllBytes());
                     }
-                    zos.putNextEntry(new ZipEntry(path));
-                    int length;
-                    while ((length = fis.read(buffer)) > 0) {
-                        zos.write(buffer, 0, length);
-                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         } catch (IOException e) {
